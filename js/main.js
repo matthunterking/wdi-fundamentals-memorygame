257cf2d2242eb7a6cd165a833f942651ae74f88a
Matt Hunter-King <matthunterking@Matts-MacBook-Air.local>
2018-02-18 17:28:38 +0000

    created more game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-console.log("Up and Running!");
+console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+cardsInPlay[0]);
-console.log("User flipped "+cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
+var cardId;
+//Check for match funtion
+var checkForMatch = function() {}
 
-	if (cardsInPlay[0]===cardsInPlay[1]) {
+		if (cardsInPlay[0]===cardsInPlay[1]) {
 		alert("You've found a match");}
 
 		else {
 			alert("Sorry try again");
-	}
+		}
+	;
+
+//End of check for match
+//Flip card function
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+    checkForMatch();
+}
 };
+flipCard(0);
+flipCard(1);
+//End of flip card function
